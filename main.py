168a463b04505b1d5fe5caa883eaf7019cd92055
Dev_fakhri <fakre19964@gmail.com>
2026-08-21 02:33:32 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import contextlib
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import create_db_tables, seed_db
 from router.books import router as books_router
@@ -24,6 +25,14 @@ app = FastAPI(
     title=general_settings.APP_TITLE,
     version=general_settings.VERSION,
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
